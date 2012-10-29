cb4228d16b9ffc783ffe96b8f8452054cec0af02
Hendy Irawan <hendy@soluvas.com>
2012-10-29 16:47:25 +0700

    HeroPanel properties Incomplete - task : Pax Wicket Localization messages

---
+++ app/src/main/java/com/hendyirawan/paxwicketl10n/app/HeroPanel.java
@@ -0,0 +1,18 @@
+package com.hendyirawan.paxwicketl10n.app;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ * Panel to demonstrate localization in Wicket component.
+ * 
+ * @author ceefour
+ * 
+ */
+@SuppressWarnings("serial")
+public class HeroPanel extends Panel {
+
+	public HeroPanel(String id) {
+		super(id);
+	}
+
+}
--- app/src/main/java/com/hendyirawan/paxwicketl10n/app/HomePage.java
+++ app/src/main/java/com/hendyirawan/paxwicketl10n/app/HomePage.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.hendyirawan.paxwicketl10n.app;
 
 import org.apache.wicket.markup.html.WebPage;
@@ -9,6 +6,12 @@ import org.apache.wicket.markup.html.WebPage;
  * @author ceefour
  *
  */
+@SuppressWarnings("serial")
 public class HomePage extends WebPage {
+
+	public HomePage() {
+		super();
+		add(new HeroPanel("hero"));
+	}
 
 }
